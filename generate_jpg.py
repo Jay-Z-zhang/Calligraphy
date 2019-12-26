c3315d22f5bf89ac9700f16d24948b7d567cfba6
Jay-Z-zhang <zhangzhi@atyun.net>
2019-12-26 11:59:55 +0800

    using flower classification, each(3 sorts) 484 each 484

--- generate_jpg.py
+++ generate_jpg.py
@@ -3,8 +3,8 @@ from keras.preprocessing.image import ImageDataGenerator
 
 datagen = ImageDataGenerator(
     rotation_range=5,  # 角度值，0~180，图像旋转
-    width_shift_range=0.2,  # 水平平移，相对总宽度的比例
-    height_shift_range=0.2,  # 垂直平移，相对总高度的比例
+    width_shift_range=0.1,  # 水平平移，相对总宽度的比例
+    height_shift_range=0.1,  # 垂直平移，相对总高度的比例
     shear_range=0.2,  # 随机错切换角度
     zoom_range=0.2,  # 随机缩放范围
     horizontal_flip=False,  # 一半图像水平翻转
@@ -13,13 +13,37 @@ datagen = ImageDataGenerator(
 
 
 train_generator = datagen.flow_from_directory(
-    'C://Users/Administrator/Desktop/Workspace/python/calligraphy/point/left_str', # 目标目录 r'C://Users/Administrator/Desktop/Workspace/python/calligraphy/point/left_gen'
-    target_size=(150, 150), # 所有图像调整为150x150
-    batch_size=1,
+    r'C://Users/Administrator/Desktop/Atyun/mozhou/Atyuncalligraphy/data/huifengbudaowei',
+# 'C://Users/Administrator/Desktop/Atyun/mozhou/Atyuncalligraphy/data/henghuajiangying/henghuajiangying_org', # 目标目录 r'C://Users/Administrator/Desktop/Workspace/python/calligraphy/point/left_gen'
+    target_size=(400, 400), # 所有图像调整为150x150
+    batch_size=5,
     shuffle=False,
-    save_to_dir=r'C://Users/Administrator/Desktop/Workspace/python/calligraphy/point/left_str/left_str_gen',
-    save_prefix='left_str',
+    save_to_dir=r'C://Users/Administrator/Desktop/Atyun/mozhou/Atyuncalligraphy/data/huifengbudaowei/huifengbudaowei_gen',
+    save_prefix='gen',
     save_format='jpg') # 二进制标签，我们用了binary_crossentropy损失函数
 
-for i in range(1000):
+
+count = 0
+for i in range(100):
     train_generator.next()
+    count += 1
+    if count % 10 == 0:
+        print("Finish amount of :" + str(count) + "%")
+
+    '''
+    # 找到本地生成图，把9张图打印到同一张figure上
+
+name_list = glob.glob(gen_path+'16/*')
+
+fig = plt.figure()
+
+for i in range(9):
+
+img = Image.open(name_list[i])
+
+sub_img = fig.add_subplot(331 + i)
+
+sub_img.imshow(img)
+
+plt.show()
+    '''
--- testcode/unit_split.py
+++ testcode/unit_split.py
@@ -123,5 +123,3 @@ if __name__ == "__main__":
 
     cv2.destroyAllWindows()
     pass
-
-
